0d69220ac5ecd1425a676d0b01dd0aea6414ed55
Edison Manrique <edisonthomas1503@gmail.com>
2021-01-10 23:52:06 -0600

    Creation of basic calculator interface

--- Calculator/src/Operations/BCalculator.java
+++ Calculator/src/Operations/BCalculator.java
@@ -1,21 +1,8 @@
+package Operations;
+
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * @author Edison Manrique @edmanrique
  */
-package calculator;
+public class BCalculator {
 
-/**
- *
- * @author Edison Manrique
- */
-public class Calculator {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
 }
