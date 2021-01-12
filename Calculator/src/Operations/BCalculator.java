c2abd01426ac968878607d369e65052265020b4f
Edison Manrique <edisonthomas1503@gmail.com>
2021-01-11 20:24:20 -0600

    New features

--- Calculator/src/Operations/BCalculator.java
+++ Calculator/src/Operations/BCalculator.java
@@ -4,5 +4,21 @@ package Operations;
  * @author Edison Manrique @edmanrique
  */
 public class BCalculator {
+    
+    public double add(double n1, double n2){
+        return n1 + n2;
+    }
+    
+    public double substract(double n1, double n2){
+        return n1 - n2;
+    }
+    
+    public double multiplication(double n1, double n2){
+        return n1 * n2;
+    }
+    
+    public double division(double n1, double n2){
+        return n1 / n2;
+    }
 
 }
